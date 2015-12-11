27dfd94910e1b8e4675bfacd4d0d3f71ba1e032e
Michael DeNatale <mikedeeno84@gmail.com>
2015-12-11 16:11:07 -0500

    first commit

---
+++ server/auth/google.oauth.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var router = require('express').Router();
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+var User = require('../api/users/user.model');
+
+router.get('/', passport.authenticate('google', {
+	scope: 'email'
+}));
+
+router.get('/callback', passport.authenticate('google', {
+	successRedirect: '/stories',
+	failureRedirect: '/signup'
+}));
+
+passport.use(new GoogleStrategy({
+	clientID: '836673402635-7o2d5nh6np5r5lintotvkcph3inmkfin.apps.googleusercontent.com',
+	clientSecret: 'HuAbDEYu61C_7xoVWtIGrb1V',
+	callbackURL: 'http://127.0.0.1:8080/auth/google/callback'
+}, function (token, refreshToken, profile, done) { 
+	User.findOne({'google.id': profile.id }, function (err, user) {
+		if (err) done(err);
+		else if (user) done(null, user);
+		else {
+			var email = profile.emails[0].value;
+			User.create({
+				email: email,
+				photo: profile.photos[0].value,
+				name: profile.displayName,
+				google: {
+					id: profile.id,
+					name: profile.displayName,
+					email: email,
+					token: token
+				}
+			}, done);
+		}
+	});
+}));
+
+module.exports = router;
